04505efa9b5482ce68051f59af496528bbe6f928
ricocatford <s7funoob@gmail.com>
2023-04-06 16:50:19 +0100

    Added test products and categories

---
+++ products/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_display_name(self):
+        return self.display_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=12, null=True, blank=True)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=64, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
