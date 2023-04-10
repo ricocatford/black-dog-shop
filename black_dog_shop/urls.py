761b71a36816239f8cc6f3453acec05181be7bcc
ricocatford <s7funoob@gmail.com>
2023-04-10 16:20:50 +0100

    Added products page, including views, templates and urls

--- black_dog_shop/urls.py
+++ black_dog_shop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/admin.py
+++ products/admin.py
@@ -13,7 +13,7 @@ class ProductAdmin(admin.ModelAdmin):
         'image'
     )
 
-    ordering = ('name',)
+    ordering = ('sku',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
