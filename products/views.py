099cd48222a78cb7679031b854011ea1e70fd6ec
ricocatford <s7funoob@gmail.com>
2023-04-11 13:10:40 +0100

    Added function for sorting products by price

--- products/views.py
+++ products/views.py
@@ -10,16 +10,31 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    categories = None
+    category = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            category = request.GET['category'].split(',')
+            products = products.filter(category__name__in=category)
+            category = Category.objects.filter(name__in=category)
 
         if 'q' in request.GET:
             query = request.GET['q']
+
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
@@ -27,10 +42,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
+        'current_category': category,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'products/products.html', context)
