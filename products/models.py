a7d52adb7e2b4609792e3e5e5929ec5adfcbc4d4
ricocatford <s7funoob@gmail.com>
2023-04-11 20:32:21 +0100

    Added shopping bag rendering items

--- products/models.py
+++ products/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=12, null=True, blank=True)
     name = models.CharField(max_length=128)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
