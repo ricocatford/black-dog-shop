adca723b34f60f23389d65d0e2e44acbe9dd9512
ricocatford <s7funoob@gmail.com>
2023-04-11 16:34:03 +0100

    Added add to shopping bag function

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ A view for rendering shopping bag """
 
     return render(request, 'bag/shopping-bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add products to the bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+
+    return redirect('products')
