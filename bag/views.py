923068697476df9d83e3f47308f5a835ad0f00b8
ricocatford <s7funoob@gmail.com>
2023-04-12 01:10:00 +0100

    Added functionality for handling sizes in shopping bag

--- bag/views.py
+++ bag/views.py
@@ -11,12 +11,25 @@ def add_to_bag(request, item_id):
     """ Add products to the bag """
 
     quantity = int(request.POST.get('quantity'))
+    size = None
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
 
