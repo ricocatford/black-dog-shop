9e2d7134ef96441707f930a80047af18fcaf61b5
ricocatford <s7funoob@gmail.com>
2023-04-25 23:20:35 +0100

    Fixed syntax error which was causing internal server error during registration

--- black_dog_shop/settings.py
+++ black_dog_shop/settings.py
@@ -209,10 +209,10 @@ STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = config('STRIPE_WH_SECRET')
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend',
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'info@blackdogshop.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend',
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
