aa7991cc52e54422d64dfca8efb3fdc8ad47df07
ricocatford <s7funoob@gmail.com>
2023-04-25 21:27:10 +0100

    Added sending emails from gmail

--- black_dog_shop/settings.py
+++ black_dog_shop/settings.py
@@ -102,8 +102,6 @@ TEMPLATES = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -210,3 +208,15 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = config('STRIPE_WH_SECRET')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend',
+    DEFAULT_FROM_EMAIL = 'info@blackdogshop.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend',
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
