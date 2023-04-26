c5e5b624c647627020c81e13daee00452e3fa829
ricocatford <s7funoob@gmail.com>
2023-04-26 02:40:15 +0100

    Fixed bug where users could not place orders if they weren't logged in (anonymous users)

--- checkout/views.py
+++ checkout/views.py
@@ -139,9 +139,9 @@ def checkout_success(request, order_number):
 
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-
-    profile = UserProfile.objects.get(user=request.user)
-    order.user_profile = profile
+    if request.user.is_anonymous is False:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
     order.save()
 
     if save_info:
